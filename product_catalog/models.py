f09dad466d23158fbe35685a57b965bb809e1c6d
Vladan Grubesic <vladan@codestudio.hr>
2022-07-02 12:46:34 +0200

    add Product model and create migration

--- product_catalog/models.py
+++ product_catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(
+        blank=False,
+        null=False,
+        unique=True,
+        max_length=255
+    )
+    price = models.FloatField(blank=False, null=False)
+    rating = models.FloatField(blank=False, null=False, validators=[
+        MinValueValidator(0.0), MaxValueValidator(5.0)
+    ])
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
